4beb888c160666f4941290ac1f68a77e6dd9e4ac
Jon Wood <jon@blankpad.net>
2010-12-07 11:38:18 +0000

    Change page caching to HTTP caching for Heroku

--- app/controllers/named_routes_controller.rb
+++ app/controllers/named_routes_controller.rb
@@ -1,10 +1,11 @@
 class NamedRoutesController < ActionController::Base
-  caches_page :generate
-
   self.view_paths = File.join(File.dirname(__FILE__), '../views/named_routes')
   layout nil
 
   def generate
+    # Cache the output for 1 year. The cache gets flushed when Heroku redeploys,
+    # so new versions will still get displayed on deployment.
+    response.headers['Cache-Control'] = 'public, max-age=1314000' if Rails.env.production?
     respond_to do |format|
       format.js { render :template => 'generate' }
     end
